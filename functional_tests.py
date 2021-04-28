01c6a6ef3a2145d15ca2a2c0e80327c69e70b132
nichita.morcotilo <n.morcotilo@support.999.md>
2021-04-28 18:42:58 +0300

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
@@ -32,7 +33,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathres' for row in rows)
+            any(row.text == '1: Buy peacock feathres' for row in rows),
+            "New to-do item did not appear in table"
         )
 
 
